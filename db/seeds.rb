3366adb0e1f32ea4a2230752ad686f11883717d1
Tony Thayil <tonyvince7@gmail.com>
2024-02-12 05:24:41 +0100

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+50.times do |i|
+  User.create!(
+    name: "User #{i + 1}",
+    email: "user#{i + 1}@example.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
