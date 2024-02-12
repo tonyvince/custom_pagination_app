3bef9967965cda84e36285d0a456d47de298573f
Tony Thayil <tonyvince7@gmail.com>
2024-02-12 05:26:45 +0100

    Add namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users, path: '', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: 'signup'
   },
   controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    sessions: 'api/v1/sessions',
+    registrations: 'api/v1/registrations'
   }
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: %i[index]
+    end
+  end
 end
